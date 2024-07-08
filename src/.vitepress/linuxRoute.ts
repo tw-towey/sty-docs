a42aaa6580148b2f2edbcf3ccab374c1e45a399b
tuwei <8003376@qq.com>
2024-07-08 23:48:45 +0800

    格式整理

--- src/.vitepress/linuxRoute.ts
+++ src/.vitepress/linuxRoute.ts
@@ -1,98 +1,98 @@
 export default  [
   {
     text: "Linux 基础教程整理",
-    link: "/docs/tutorialBasis/index.md",
+    link: "/docs/linuxTutorial/index.md",
   },
   {
     text: "00. 计算机概论",
-    link: "/docs/tutorialBasis/00. 计算机概论",
+    link: "/docs/linuxTutorial/00. 计算机概论",
   },
   {
     text: "01. Linux 是什么 如何学习",
-    link: "/docs/tutorialBasis/01. Linux 是什么 如何学习.md",
+    link: "/docs/linuxTutorial/01. Linux 是什么 如何学习.md",
   },
   {
     text: "02. 主机规划与磁盘分区",
-    link: "/docs/tutorialBasis/02. 主机规划与磁盘分区.md",
+    link: "/docs/linuxTutorial/02. 主机规划与磁盘分区.md",
   },
   {
     text: "03. 安装 CentOS 7.x",
-    link: "/docs/tutorialBasis/03. 安装 CentOS 7.x.md",
+    link: "/docs/linuxTutorial/03. 安装 CentOS 7.x.md",
   },
   {
     text: "04. 首次登陆与线上求助",
-    link: "/docs/tutorialBasis/04. 首次登陆与线上求助.md",
+    link: "/docs/linuxTutorial/04. 首次登陆与线上求助.md",
   },
   {
     text: "05. Linux 的文件权限与目录配置",
-    link: "/docs/tutorialBasis/05. Linux 的文件权限与目录配置.md",
+    link: "/docs/linuxTutorial/05. Linux 的文件权限与目录配置.md",
   },
   {
     text: "06. Linux 文件与目录管理",
-    link: "/docs/tutorialBasis/06. Linux 文件与目录管理.md",
+    link: "/docs/linuxTutorial/06. Linux 文件与目录管理.md",
   },
   {
     text: "07. Linux 磁盘与文件系统管理",
-    link: "/docs/tutorialBasis/07. Linux 磁盘与文件系统管理.md",
+    link: "/docs/linuxTutorial/07. Linux 磁盘与文件系统管理.md",
   },
   {
     text: "08. 文件与文件系统的压缩、打包与备份",
-    link: "/docs/tutorialBasis/08. 文件与文件系统的压缩、打包与备份.md",
+    link: "/docs/linuxTutorial/08. 文件与文件系统的压缩、打包与备份.md",
   },
   {
     text: "09. vim 程序编辑器",
-    link: "/docs/tutorialBasis/09. vim 程序编辑器.md",
+    link: "/docs/linuxTutorial/09. vim 程序编辑器.md",
   },
   {
     text: "10. 认识与学习 BASH",
-    link: "/docs/tutorialBasis/10. 认识与学习 BASH.md",
+    link: "/docs/linuxTutorial/10. 认识与学习 BASH.md",
   },
   {
     text: "11. 正则表达式与文件格式化处理",
-    link: "/docs/tutorialBasis/11. 正则表达式与文件格式化处理.md",
+    link: "/docs/linuxTutorial/11. 正则表达式与文件格式化处理.md",
   },
   {
     text: "12. 学习 Shell Scripts",
-    link: "/docs/tutorialBasis/12. 学习 Shell Scripts.md",
+    link: "/docs/linuxTutorial/12. 学习 Shell Scripts.md",
   },
   {
     text: "13. Linux 账号管理与 ACL 权限设置",
-    link: "/docs/tutorialBasis/13. Linux 账号管理与 ACL 权限设置.md",
+    link: "/docs/linuxTutorial/13. Linux 账号管理与 ACL 权限设置.md",
   },
   {
     text: "14. 磁盘配额（Quota）与进阶文件系统管理",
-    link: "/docs/tutorialBasis/14. 磁盘配额（Quota）与进阶文件系统管理.md",
+    link: "/docs/linuxTutorial/14. 磁盘配额（Quota）与进阶文件系统管理.md",
   },
   {
     text: "15. 例行性工作排程（crontab）",
-    link: "/docs/tutorialBasis/15. 例行性工作排程（crontab）.md",
+    link: "/docs/linuxTutorial/15. 例行性工作排程（crontab）.md",
   },
   {
     text: "16. 进程管理与 SELinux 初探",
-    link: "/docs/tutorialBasis/16. 进程管理与 SELinux 初探.md",
+    link: "/docs/linuxTutorial/16. 进程管理与 SELinux 初探.md",
   },
   {
     text: "17. 认识系统服务（daemons）",
-    link: "/docs/tutorialBasis/17. 认识系统服务（daemons）.md",
+    link: "/docs/linuxTutorial/17. 认识系统服务（daemons）.md",
   },
   {
     text: "18. 认识与分析登录文件",
-    link: "/docs/tutorialBasis/18. 认识与分析登录文件.md",
+    link: "/docs/linuxTutorial/18. 认识与分析登录文件.md",
   },
   {
     text: "19. 开机流程、模块管理与 Loader",
-    link: "/docs/tutorialBasis/19. 开机流程、模块管理与 Loader.md",
+    link: "/docs/linuxTutorial/19. 开机流程、模块管理与 Loader.md",
   },
   {
     text: "20. 基础系统设置与备份策略",
-    link: "/docs/tutorialBasis/20. 基础系统设置与备份策略.md",
+    link: "/docs/linuxTutorial/20. 基础系统设置与备份策略.md",
   },
   {
     text: "",
-    link: "/docs/tutorialBasis/21. 软件安装：原始码与 Tarball.md",
+    link: "/docs/linuxTutorial/21. 软件安装：原始码与 Tarball.md",
   },
   {
     text: "22. 软件安装 RPM、SRPM、YUM",
-    link: "/docs/tutorialBasis/22. 软件安装 RPM、SRPM、YUM.md",
+    link: "/docs/linuxTutorial/22. 软件安装 RPM、SRPM、YUM.md",
   },
 ]
